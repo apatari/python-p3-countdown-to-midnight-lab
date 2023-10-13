cab65193e3b04bd314a2436a3a7d0abb34598cf1
Andy Patari <andy.patari@gmail.com>
2023-10-13 13:30:30 -0400

    passing tests

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,15 @@
 # your code goes here!
+import time
+
+def countdown(n):
+    while n:
+        print(f'{n} SECOND(S)!')
+        n -= 1
+    print('HAPPY NEW YEAR!')
+
+def countdown_with_sleep(n):
+    while n:
+        print(f'{n} SECOND(S)!')
+        n -= 1
+        time.sleep(1)
+    print('HAPPY NEW YEAR!')
